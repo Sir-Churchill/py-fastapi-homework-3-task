123f4c8a511e3f7687984191d283985c670d267c
Sir-Churchill <trickster@MacBook-Air-Trickster.local>
2025-11-09 22:04:02 +0100

    Solution Fix

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -196,10 +196,10 @@ async def refresh_token(
     db_user = result.scalar_one_or_none()
 
     if not db_user:
-        raise HTTPException(status_code=401, detail="User not found.")
+        raise HTTPException(status_code=401, detail="Refresh token not found.")
 
     if not db_user.refresh_tokens:
-        raise HTTPException(status_code=401, detail="Refresh token not found.")
+        raise HTTPException(status_code=404, detail="User not found.")
 
     token_obj = next((t for t in db_user.refresh_tokens if t.token == user.refresh_token), None)
 
